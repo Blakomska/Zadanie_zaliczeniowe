19e87c388a02d31808dc721a57d1fcce390b0c5b
Blakomska <blanka.lakomska@gmail.com>
2026-03-16 20:00:50 +0100

    Dodanie testu e2e

--- pages/zaliczenie_produkt.js
+++ pages/zaliczenie_produkt.js
@@ -3,6 +3,15 @@ import { expect } from "@playwright/test";
 export class Produkt { 
     constructor(page) {
         this.page = page;
+        this.url = 'https://mad-qa.pl/';
+
+        this.productTitle = this.page.getByTestId('product-title-7');
+        this.buyButton = this.page.getByTestId('buy-btn-7');
+        this.successAddMessage = this.page.getByText('Dodano do koszyka: Notes QA');
+        this.busketButton = this.page.getByTestId('cart-button');
+        this.cartBuy = this.page.getByTestId('cart-buy');
+        this.cartBuyButton = this.page.getByTestId('cart-buy');
+        this.successBuyMessage = this.page.getByText('sukces');
 
 }
 }
--- tests/zadanie_zaliczeniowe.spec.js
+++ tests/zadanie_zaliczeniowe.spec.js
@@ -1,11 +1,13 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-import { Zaliczenie } from '../pages/zaliczenie_page';  
+import { Zaliczenie } from '../pages/zaliczenie_page';
+
+import { Produkt } from '../pages/zaliczenie_produkt';
 
 test('Zaliczenie - test logowania', async ({ page }) => {
 
     const zaliczenie = new Zaliczenie(page);
-   
+
     const correctUsername = process.env.USER_NAME;
     const correctPassword = process.env.USER_PASSWORD;
 
@@ -20,5 +22,22 @@ test('Zaliczenie - test logowania', async ({ page }) => {
 
     await expect(zaliczenie.welcomeMessage).toBeVisible();
     await expect(zaliczenie.errorMessage).toBeHidden();
+});
+
+test('Wybranie produktu i dodanie do koszyka', async ({ page }) => {
+
+    const produkt = new Produkt(page);
+
+    await page.goto(produkt.url);
+    await expect(produkt.productTitle).toBeVisible();
+    await produkt.productTitle.click();
+    await expect(produkt.buyButton).toBeVisible();
+    await produkt.buyButton.click();
+    
+    await expect(produkt.successAddMessage).toBeVisible();
+    await produkt.busketButton.click();
+    await expect(produkt.cartBuy).toBeVisible();
+    await produkt.cartBuyButton.click();
+    await expect(produkt.successBuyMessage).toBeVisible();
 });
 
